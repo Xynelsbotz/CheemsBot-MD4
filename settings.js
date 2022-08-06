b0b9aa2c8adbd581ba9146a0aed4ff60e08413a8
Xynels Design <107124409+Xynelsbotz@users.noreply.github.com>
2022-08-06 14:03:46 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['6285854337533'] //ur owner number
+global.ownername = "🎫 Xynels Design" //ur owner name
+global.ytname = "YT: Xynels Design" //ur yt chanel name
+global.socialm = "GitHub: Xynelsbotz" //ur github or insta name
+global.location = "Indonesia, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['6285854337533']
+global.ownertag = '6285854337533' //ur tag number
+global.botname = 'Sayu-Ogiwara🌸' //ur bot name
+global.linkz = "https://xynelsdesign.carrd.co/" //your theme url which will be displayed on whatsapp
+global.websitex = "https://github.com/Xynelsbotz" //ur website to be displayed
+global.botscript = '(PRIVATE)' //script link
+global.reactmoji = "😎" //ur menu react emoji
+global.themeemoji = "🎫" //ur theme emoji
+global.packname = "Sayu Bot Stickers\n\n\n\n\n\n\n\n\n\n🎫🎫🎫 " //ur sticker watermark packname
+global.author = "🎫 Xynels Design" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['6285854337533'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -101,7 +101,7 @@ global.ntilink = []
 
 //omther
 global.sessionName = 'session'
-global.antitags = true
+global.antitags = false
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
